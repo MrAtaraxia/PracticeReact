f4839d2279c4a9a3efb09849b79afddfdbf5792b
Will Chnowski <william.chnowski@gmail.com>
2021-11-24 22:06:38 -0500

    adjusting for testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import Controller from './features/controller/Controller';
 import Navigation from './features/navigation/Navigation';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 type props = {}
 
 const App:React.FC<props> = () => {
   return (
-    <div className="App">
-      <Controller><Navigation/>abc</Controller>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Controller><Navigation/>abc</Controller>
+      </div>
+    </Provider>
   );
 }
 
